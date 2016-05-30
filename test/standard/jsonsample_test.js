7bc2f3eabb0b161dab71ec6edc21cc5aaa02b8ba
Didier Plaindoux <d.plaindoux@free.fr>
2016-05-30 07:45:28 +0200

    Add some performance tests

---
+++ test/standard/jsonsample_test.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var fs = require('fs'),
+    jsonparser = require('../../lib' + (process.env.COVERAGE || '') + '/standard/jsonparser.js'),
+    stream = require('../../lib' + (process.env.COVERAGE || '') + '/stream/streams.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+function sampleTest(sample, test) {
+    test.expect(1);
+    
+    // tests here  
+    fs.readFile('./test/standard/samples/' + sample, function (err,data) {
+        if (err) {
+            throw err;
+        }
+        
+        var result = { isAccepted : function() { return false; } };
+        
+        try {
+            result = jsonparser.parse(stream.ofString(data.toString()));
+        } catch (e) {
+            console.log(e.stack); 
+        }
+        
+        if (!result.isAccepted()) {        
+            console.log(result);
+        }  
+
+        test.ok(result.isAccepted(), "Well formed JSON");
+        test.done();                
+    });    
+}
+
+exports['language_analyse'] = {
+  setUp: function(done) {
+    done();
+  },
+
+  'test 1k': function(test) {
+    sampleTest("1k.json", test);    
+  },
+    
+  'test 100k': function(test) {
+    sampleTest("100k.json", test);    
+  },
+};
