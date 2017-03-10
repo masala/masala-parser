b8cec347c3dcd80edb05329bcb431345f1ca4dab
Nicolas Zozol <nzozol@gmail.com>
2017-03-10 15:06:32 +0100

    Closes #30 - integration show ease of use for parsing string

--- integration/integrate.js
+++ integration/integrate.js
@@ -1,4 +1,12 @@
-const parsec = require('./parser-combinator.min');
+const {stream,F,C,N,} = require('../dist/parser-combinator.min');
+
+const st = stream.ofString('12');
+const parsing = N.numberLiteral.thenLeft(F.eos).parse(st);
+
+console.log(parsing.value===12);
+
+/*
+//import {F,C,N} from 'parsec/index';
 const P = parsec.parser;
 const T= parsec.standard.token;
 
@@ -9,4 +17,4 @@ const document = '12';
 const stream = parsec.stream.ofString(document);
 
 const parsing = P.numberLiteral.thenLeft(P.eos).parse(stream);
-console.log(parsing.value===12);
+console.log(parsing.value===12);*/
