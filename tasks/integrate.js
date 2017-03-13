57e38654b8fdc03b6cd874238429d118a543df3c
Nicolas Zozol <nzozol@gmail.com>
2017-03-13 22:00:21 +0100

    #22: prepublish tests are ok

--- tasks/integrate.js
+++ tasks/integrate.js
@@ -1,12 +1,8 @@
-const parsec = require('./parser-combinator.min');
-const P = parsec.parser;
-const T= parsec.standard.token;
+const {stream,F,C,N,} = require('../dist/parser-combinator.min');
 
+const st = stream.ofString('12');
+const parsing = N.numberLiteral.thenLeft(F.eos).parse(st);
 
-console.log(parsec.parser);
-
-const document = '12';
-const stream = parsec.stream.ofString(document);
-
-const parsing = P.numberLiteral.thenLeft(P.eos).parse(stream);
-console.log(parsing.value===12);
+if (parsing.value !== 14){
+    throw "Illegal value parsed in prepublish integration test";
+}
