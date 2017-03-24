9a571266f8562bec78fd0e06d9e91db9161d093d
Nicolas Zozol <nzozol@gmail.com>
2017-03-24 16:07:55 +0100

    0.3 integration with older javascript

--- tasks/integrate.js
+++ tasks/integrate.js
@@ -1,4 +1,9 @@
-const {stream,F,C,N,} = require('../dist/parser-combinator.min');
+//{stream,F,C,N,}
+const parsec = require('../dist/parser-combinator.min');
+const stream = parsec.stream;
+const F = parsec.F;
+const N=parsec.N;
+
 
 const st = stream.ofString('12');
 const parsing = N.numberLiteral.thenLeft(F.eos).parse(st);
