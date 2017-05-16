dd50ca6166fe4817024b12872e04da59768c6475
Didier Plaindoux <d.plaindoux@free.fr>
2017-05-16 07:00:29 +0200

    Update list.js

--- src/lib/data/list.js
+++ src/lib/data/list.js
@@ -1,6 +1,6 @@
 /*
  * Parsec
- * https://github.com/d-plaindoux/thicket
+ * https://github.com/d-plaindoux/parsec
  *
  * Copyright (c) 2015-2016 Didier Plaindoux
  * Licensed under the LGPL2 license.
