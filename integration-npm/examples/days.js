d31dd99b7b3ef77d6f42921ae229396408eb1173
Nicolas Zozol <nzozol@gmail.com>
2017-10-10 09:32:30 +0200

    Integration Test; Operation: Complex lazy recursion with days example

--- integration-npm/examples/days.js
+++ integration-npm/examples/days.js
@@ -1,63 +1,69 @@
 const {stream, F, N, C, X} = require('parser-combinator');
 
+/*
+ Implementing stack solution :
+ E -> T E'
+ E' -> + TE'  |  eps
+ T -> F T'
+ T' -> * FT'  |  eps
+ F -> NUMBER | ID | ( E )
+
+ Using only one operator, simplified to:
+
+ T -> F T'
+ T' -> * FT'  |  eps
+ F -> NUMBER | ID | ( T )
+
+
+ Translated to :
+ expr -> terminal subExpr.opt()
+ subExpr -> F.try(operator terminal subExpr )
+ terminal -> DAY | ( expr )
+
+ */
+
 
 function day() {
-     const x = new X()
+    const x = new X()
     return x.stringIn(['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY']);
-    //return C.string('MONDAY').or(C.string('TUESDAY')).or(C.string('WEDNESDAY'));
 }
 
 
-/* Idiomatic recursive expression */
-function terminal() {
-    return day();
+function operation() {
+    return F.try(C.string(' AND ').debug('AND operator')).or(C.string(' OR ').debug('OR operator'))
 }
 
 
-
-function operation(){
-        return F.try(C.string(' AND ')).or(C.string(' OR '))
-}
-
-
-
-function parenthesis(par){
+function parenthesis(par) {
     return C.char(' ').optrep().drop().then(C.char(par)).then(C.char(' ').optrep().drop());
 }
 
-function parenthesisExpr(){
-    return parenthesis('(').drop().then(expr()).then(parenthesis(')').drop());
-}
-
-
-// Implementing basic solution :
-//exp   = sexp (op exp)?
-//sexp = number | '(' exp ')'
-
-function subexpr(){
-    return terminal().or(parenthesisExpr());
-}
-
-function operationThenExpression(){
-    return operation().then(expr());
+function parenthesisExpr() {
+    return parenthesis('(').debug('(').drop().then(expr().debug('inside parenthesis expression')).then(parenthesis(')').drop());
 }
 
 
 
 function expr() {
-
-    return subexpr().then(operationThenExpression().opt());
-
+    return terminal().debug('expr terminal').then(subExpr().opt())
 }
 
+function subExpr() {
+    return F.try(operation().then(terminal()).debug('terminal after operation').then(F.lazy(subExpr).opt().debug('optionnel subExpr')));
+}
+
+
+function terminal() {
+    return day().debug('day').or(F.lazy(parenthesisExpr).debug('parenthesis expression')).debug('after terminal OR');
+}
 
 
 function combinator() {
-    return expr();
+    return expr().debug('expr');
 }
 
 
-const string = 'TUESDAY OR WEDNESDAY';
+const string = 'TUESDAY AND (WEDNESDAY OR FRIDAY)';
 
 let myStream = stream.ofString(string);
 let parsing = combinator().parse(myStream);
