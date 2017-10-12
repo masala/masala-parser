ad13cdb2196e399f13d9d09530259a2ea2452723
Nicolas Zozol <nzozol@gmail.com>
2017-10-12 16:53:05 +0200

    #43: Adding C.letterAs to test occidental letters, utf8 or just ascii.

--- src/test/parsec/chars-bundle-test.js
+++ src/test/parsec/chars-bundle-test.js
@@ -1,5 +1,5 @@
 import stream from '../../lib/stream/index';
-import {C} from '../../lib/parsec/index';
+import {F, C} from '../../lib/parsec/index';
 
 let value = undefined;
 
@@ -11,11 +11,360 @@ function testParser(parser, string) {
 }
 
 export default {
-    setUp: function(done) {
+    setUp: function (done) {
         done();
     },
 
-    'expect accent to be accepted': function(test) {
+    'expect (char) to be accepted': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(
+            C.char('a').parse(stream.ofString('a'), 0).isAccepted(),
+            true,
+            'should be accepted.'
+        );
+        test.done();
+    },
+
+    'expect (char) to be rejected': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(
+            C.char('a').parse(stream.ofString('b'), 0).isAccepted(),
+            false,
+            'should be rejected.'
+        );
+        test.done();
+    },
+
+    'expect (char) to be refused': function (test) {
+        test.expect(1);
+        // tests here
+        test.throws(function () {
+            C.char('aa');
+        });
+        test.done();
+    },
+
+    'expect (notChar) to be accepted': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(
+            C.notChar('a').parse(stream.ofString('b'), 0).isAccepted(),
+            true,
+            'should be accepted.'
+        );
+        test.done();
+    },
+
+    'expect (notChar) to be rejected': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(
+            C.notChar('a').parse(stream.ofString('a'), 0).isAccepted(),
+            false,
+            'should be rejected.'
+        );
+        test.done();
+    },
+
+    'expect (notChar) to be refused': function (test) {
+        test.expect(1);
+        // tests here
+        test.throws(function () {
+            C.notChar('aa');
+        });
+        test.done();
+    },
+
+    'expect (charNotIn) to be accepted': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(
+            C.charNotIn('a').parse(stream.ofString('b'), 0).isAccepted(),
+            true,
+            'should be accepted.'
+        );
+        test.done();
+    },
+
+    'expect (charNotIn) to be rejected': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(
+            C.charNotIn('a').parse(stream.ofString('a'), 0).isAccepted(),
+            false,
+            'should be rejected.'
+        );
+        test.done();
+    },
+
+    'expect (charIn) to be accepted': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(
+            C.charIn('a').parse(stream.ofString('a'), 0).isAccepted(),
+            true,
+            'should be accepted.'
+        );
+        test.done();
+    },
+
+    'expect (charIn) to be rejected': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(
+            C.charIn('a').parse(stream.ofString('b'), 0).isAccepted(),
+            false,
+            'should be rejected.'
+        );
+        test.done();
+    },
+
+
+    'expect (lowerCase) to be accepted': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(
+            C.lowerCase.parse(stream.ofString('a'), 0).isAccepted(),
+            true,
+            'should be accepted.'
+        );
+        test.done();
+    },
+
+    'expect (lowerCase) to be rejected': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(
+            C.lowerCase.parse(stream.ofString('A'), 0).isAccepted(),
+            false,
+            'should be rejected.'
+        );
+        test.done();
+    },
+
+    'expect (upperCase) to be accepted': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(
+            C.upperCase.parse(stream.ofString('A'), 0).isAccepted(),
+            true,
+            'should be accepted.'
+        );
+        test.done();
+    },
+
+    'expect (upperCase) to be rejected': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(
+            C.upperCase.parse(stream.ofString('z'), 0).isAccepted(),
+            false,
+            'should be rejected.'
+        );
+        test.done();
+    },
+
+    'expect upper (letter) to be accepted': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(
+            C.letter.parse(stream.ofString('A'), 0).isAccepted(),
+            true,
+            'should be accepted.'
+        );
+        test.done();
+    },
+
+    'expect lower (letter) to be accepted': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(
+            C.letter.parse(stream.ofString('z'), 0).isAccepted(),
+            true,
+            'should be accepted.'
+        );
+        test.done();
+    },
+    'expect space (letter) to be rejected': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(
+            C.letter.parse(stream.ofString(' '), 0).isAccepted(),
+            false,
+            'should be rejected.'
+        );
+        test.done();
+    },
+
+    'expect non (letter) to be rejected': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(
+            C.letter.parse(stream.ofString('0'), 0).isAccepted(),
+            false,
+            'should be rejected.'
+        );
+        test.done();
+    },
+
+    'expect occidental letter to be accepted': function (test) {
+
+        test.ok(C.letter.parse(stream.ofString('a'), 0).isAccepted());
+        test.ok(C.letterAs().parse(stream.ofString('a'), 0).isAccepted());
+        test.ok(C.letterAs(C.OCCIDENTAL_LETTER).parse(stream.ofString('a')).isAccepted());
+        test.ok(C.letterAs(C.OCCIDENTAL_LETTER).parse(stream.ofString('é')).isAccepted());
+        test.ok(!C.letterAs(C.OCCIDENTAL_LETTER).parse(stream.ofString('Б')).isAccepted());
+        test.ok(!C.letterAs(C.OCCIDENTAL_LETTER).parse(stream.ofString('÷')).isAccepted());
+        test.done();
+    },
+
+    'expect occidental letters to be accepted': function (test) {
+
+        test.ok(C.letters.then(F.eos).parse(stream.ofString('aéÉ'), 0).isAccepted());
+        test.ok(C.lettersAs().then(F.eos).parse(stream.ofString('aéÉ'), 0).isAccepted());
+        test.ok(C.lettersAs(C.OCCIDENTAL_LETTER).parse(stream.ofString('a')).isAccepted());
+        test.ok(C.lettersAs(C.OCCIDENTAL_LETTER).then(F.eos).parse(stream.ofString('éA')).isAccepted());
+        test.ok(!C.lettersAs(C.OCCIDENTAL_LETTER).then(F.eos).parse(stream.ofString('БAs')).isAccepted());
+        test.done();
+    },
+
+    'expect ascii letter to be accepted': function (test) {
+
+
+        test.ok(C.letterAs(C.ASCII_LETTER).parse(stream.ofString('a'), 0).isAccepted());
+        test.ok(!C.letterAs(C.ASCII_LETTER).parse(stream.ofString('é')).isAccepted());
+        test.ok(!C.letterAs(C.ASCII_LETTER).parse(stream.ofString('Б')).isAccepted());
+        test.done();
+    },
+
+    'expect ascii letters to be accepted': function (test) {
+
+        test.ok(C.lettersAs(C.ASCII_LETTER).parse(stream.ofString('a')).isAccepted());
+        test.ok(! C.lettersAs(C.ASCII_LETTER).then(F.eos).parse(stream.ofString('éA')).isAccepted());
+        test.ok(!C.lettersAs(C.ASCII_LETTER).then(F.eos).parse(stream.ofString('БAs')).isAccepted());
+        test.done();
+    },
+
+    'expect utf8 letter to be accepted': function (test) {
+
+        test.ok(C.letterAs(C.UTF8_LETTER).parse(stream.ofString('a')).isAccepted());
+        test.ok(C.letterAs(C.UTF8_LETTER).parse(stream.ofString('é')).isAccepted());
+        test.ok(C.letterAs(C.UTF8_LETTER).parse(stream.ofString('Б')).isAccepted());
+        test.ok(!C.letterAs(C.UTF8_LETTER).parse(stream.ofString('÷')).isAccepted());
+        test.done();
+    },
+    'expect utf8 letters to be accepted': function (test) {
+
+        test.ok(C.lettersAs(C.UTF8_LETTER).then(F.eos).parse(stream.ofString('a')).isAccepted());
+        test.ok(C.lettersAs(C.UTF8_LETTER).then(F.eos).parse(stream.ofString('éA')).isAccepted());
+        test.ok(C.lettersAs(C.UTF8_LETTER).then(F.eos).parse(stream.ofString('БAs')).isAccepted());
+        test.ok(! C.letterAs(C.UTF8_LETTER).then(F.eos).parse(stream.ofString('Б÷As')).isAccepted());
+        test.done();
+    },
+
+
+    'expect (letters) to be accepted': function (test) {
+        test.expect(2);
+        // tests here
+        const parsing = C.letters
+            .thenLeft(F.eos)
+            .parse(stream.ofString('someLetters'), 0);
+        test.equal(parsing.isAccepted(), true, 'should be accepted.');
+        test.deepEqual(parsing.value, 'someLetters', 'should be equal.');
+        test.done();
+    },
+
+    'expect (letters) with space to be rejected': function (test) {
+        test.expect(2);
+        // tests here
+        const parsing = C.letters
+            .then(F.eos)
+            .parse(stream.ofString('some Letters'), 0);
+        test.equal(parsing.isAccepted(), false, 'should be rejected.');
+        test.notDeepEqual(parsing.value, 'some Letters', 'should be equal.');
+        test.done();
+    },
+
+    'expect (letters) with number to be rejected': function (test) {
+        test.expect(1);
+        // tests here
+        const parsing = C.letters
+            .then(F.eos)
+            .parse(stream.ofString('some2Letters'), 0);
+        test.equal(parsing.isAccepted(), false, 'should be accepted.');
+        test.done();
+    },
+
+    'expect (letters) to return a string, not an array of letters': function (test) {
+        test.expect(1);
+        // tests here
+        const parsing = C.letters
+            .thenLeft(F.eos)
+            .parse(stream.ofString('someLetters'), 0);
+        test.equal(parsing.value, 'someLetters', 'not a string');
+        test.done();
+    },
+
+
+    'expect (string) to be accepted': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(
+            C.string('Hello').parse(stream.ofString('Hello'), 0).isAccepted(),
+            true,
+            'should be accepted.'
+        );
+        test.done();
+    },
+
+    'expect (string) to be rejected': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(
+            C.string('hello').parse(stream.ofString('hell'), 0).isAccepted(),
+            false,
+            'should be rejected.'
+        );
+        test.done();
+    },
+
+    'expect (notString) to be accepted': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(
+            C.notString('**').parse(stream.ofString('hello'), 0).isAccepted(),
+            true,
+            'should be accepted.'
+        );
+        test.done();
+    },
+
+    'expect (notString) to be h': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(
+            C.notString('**').parse(stream.ofString('hello'), 0).value,
+            'h',
+            'should be h.'
+        );
+        test.done();
+    },
+
+    'expect (notString) to be rejected': function (test) {
+        test.expect(1);
+        // tests here
+        test.equal(
+            C.notString('**').parse(stream.ofString('**hello'), 0).isAccepted(),
+            false,
+            'should be rejected.'
+        );
+        test.done();
+    },
+
+
+    'expect accent to be accepted': function (test) {
         test.expect(1);
         // tests here
         test.equal(
@@ -25,7 +374,7 @@ export default {
         );
         test.done();
     },
-    'expect cyriliq to be accepted': function(test) {
+    'expect cyriliq to be accepted': function (test) {
         test.expect(2);
         // tests here
         //Белград
@@ -42,7 +391,7 @@ export default {
         );
         test.done();
     },
-    'expect dash to be rejected': function(test) {
+    'expect dash to be rejected': function (test) {
         test.expect(1);
         // tests here
         test.equal(
@@ -52,7 +401,7 @@ export default {
         );
         test.done();
     },
-    'expect "nothing" to be accepted': function(test) {
+    'expect "nothing" to be accepted': function (test) {
         test.expect(1);
         // tests here
         test.equal(
