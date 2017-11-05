f66958c3e915f52032d6067d3672994567aca84d
Nicolas Zozol <nzozol@gmail.com>
2017-11-05 16:38:13 +0100

    #88: TS: flatMap and lazy

--- integration-ts/examples/index.ts
+++ integration-ts/examples/index.ts
@@ -3,6 +3,8 @@ import './beginner/floor'
 import './beginner/hello-something'
 import './beginner/number'
 import './beginner/response'
+import './flow/try-with-no-or'
+import './lazy/transmission'
 
 
 
---
+++ integration-ts/examples/lazy/transmission.ts
@@ -0,0 +1,36 @@
+import {Streams, F, C, Option, N, SingleParser, parserBuilder, ListParser} from '@robusta/trash'
+import {assertTrue} from '../../assert';
+
+
+/**
+ * A gives its VALUE to B using flatMap
+ */
+function A(char){
+    return C.char(char.toUpperCase()).rep().flatMap(B);
+}
+
+
+/**
+ * There is recursion, and we call A with lazy. We send PARAMETERS to A
+ * within an array
+ */
+function B(aVal) {
+    console.log('>>>>',aVal);
+    return C.char('B').map(bVal=> aVal.join('')+'-'+bVal).or(F.lazy(A, ['a']));
+}
+
+
+
+const parser = A('a');
+
+const str = 'AAAB';
+const stream = Streams.ofString(str);
+const parsing = parser.parse(stream);
+
+assertTrue(parsing.offset === str.length);
+
+//F.startsWith()
+//F.dropTo()
+//F.moveUntil()
+// rename F.nop() to F.nop
+// parsing.isCompleted()
