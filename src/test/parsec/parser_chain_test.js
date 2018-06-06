9a6d425562282762ae3da54842c496e87741683c
Nicolas Zozol <nzozol@gmail.com>
2018-06-06 17:28:58 +0200

    #108: WIP: additional tests for parser.chain

--- src/test/parsec/parser_chain_test.js
+++ src/test/parsec/parser_chain_test.js
@@ -1,11 +1,60 @@
 import stream from '../../lib/stream/index';
 import {F, C, N} from '../../lib/parsec/index';
+import unit from "../../lib/data/unit";
 
 export default {
     setUp: function (done) {
         done();
     },
 
+    'expect (chain) to be accepted': function (test){
+
+        const lower = C.char('x');
+
+        const upper = F.satisfy( val => val ==='x' );
+
+        const parser  = lower.chain(upper);
+
+        const response = parser.parse(stream.ofString('x'));
+
+        test.ok(response.isAccepted(), 'Should be accepted');
+
+        test.done();
+
+    },
+    'expect (chain) to be rejected': function (test){
+
+        const lower = C.char('x');
+
+        const upper = F.satisfy( val => val ==='y' );
+
+        const parser  = lower.chain(upper);
+
+        const response = parser.parse(stream.ofString('x'));
+
+        test.ok(! response.isAccepted(), 'Should be rejected');
+
+        test.done();
+
+    },
+    'expect (chain) to be accepted and offset to have move': function (test){
+
+        const lower = C.char('x');
+
+        // satisfy makes the stuff move only if accepted
+        const upper = F.satisfy( val => val ==='x' );
+
+        const parser  = lower.chain(upper);
+
+        const response = parser.parse(stream.ofString('x'));
+
+        console.log(response.offset)
+        test.ok( response.offset, 'Should be rejected');
+
+        test.done();
+
+    }
+/*
     'expect (chain) to be accepted': function (test) {
         test.expect(1);
         // tests here
@@ -36,5 +85,36 @@ export default {
             'should be 46.'
         );
         test.done();
-    }
+    },
+
+    'expect (chain) to fail ': function (test) {
+        const token = N.numberLiteral();
+        const lex = spaces().drop().debug('start')
+            .then(F.any()).debug('any')
+            .then(spaces().drop().debug('end'))
+            .then(F.any()).debug('any2')
+            .map(values => values.array().reduce((acc, n) => {
+                    console.log(values, acc, n)
+                    return acc + n
+                }
+                , 0
+            ));
+
+
+        const parsing = token.chain(lex).parse(stream.ofString('10 12 '), 0)
+
+        console.log(parsing.isConsumed())
+
+        test.equal(
+            parsing.value,
+            20,
+            'should be 20.'
+        );
+        test.done();
+
+    }*/
+}
+
+function spaces() {
+    return C.charIn(' \r\n\f\t').optrep().map(() => unit);
 }
