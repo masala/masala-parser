e8d42b942815d2f27c356e7badab66218931d895
Nicolas Zozol <nzozol@gmail.com>
2019-05-13 15:15:54 +0200

    v0.8 genlex cover

--- src/test/genlex/genlex-package-test.js
+++ src/test/genlex/genlex-package-test.js
@@ -1,11 +1,13 @@
 import genlexTest from './genlex-test';
 import genlexOperationTest from './genlex-operation';
 import propositionTest from './proposition';
+import brainfuck from './genlex-brainfuck-test';
 
 export default {
     genlexTest,
     genlexOperationTest,
-    propositionTest
+    propositionTest,
+    brainfuck
 }
 
 
