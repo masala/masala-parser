068bca4d84f90b29de459e61378871c3f2772de9
Nicolas Zozol <nzozol@gmail.com>
2025-05-18 21:57:28 +0200

    demo(v2): demo start

--- demo/src/demo.spec.ts
+++ demo/src/demo.spec.ts
@@ -19,9 +19,9 @@ year: 2023
 
 const leftText = F.regex(/[a-zA-Z_][a-zA-Z0-9_]*/)
 const separator = C.char(':')
-const rightText = F.moveUntil(C.char('\n'))
+const rightText = F.moveUntil(C.char('\n').drop(), true)
 
-const line = leftText
+const lineParser = leftText
     .then(separator.drop())
     .then(rightText)
     .array()
@@ -30,15 +30,28 @@ const line = leftText
         value: value.trim(),
     }))
 
+const documentParser = lineParser.rep().then(C.string('---').drop()).array()
+
 describe('Parser Combinator demonstration', () => {
     it('should parse a line', () => {
         const stream = Streams.ofString(document)
-        const parsing = line.parse(stream)
+        const parsing = lineParser.parse(stream)
         expect(parsing.isAccepted()).toBe(true)
         expect(parsing.value).toEqual({
             name: 'author',
             value: 'Nicolas',
         })
         expect(parsing.isEos()).toBe(false)
+    })
+
+    it('should parse the entire front matter', () => {
+        const stream = Streams.ofString(document)
+        const parsing = documentParser.parse(stream)
+        expect(parsing.isAccepted()).toBe(true)
+        expect(parsing.value).toEqual([
+            { name: 'author', value: 'Nicolas' },
+            { name: 'purpose', value: 'Demo of Masala Parser' },
+            { name: 'year', value: '2023' },
+        ])
     })
 })
