811e007e76bdbec41e49c23f4d243b9bb686e6b7
Nicolas Zozol <912634+nicolas-zozol@users.noreply.github.com>
2025-10-24 21:32:05 +0100

    fix(v2): removing noise from v1

--- tasks/post-compile.js
+++
@@ -1,6 +0,0 @@
-import  ncp from 'ncp';
-
-console.log('\n\n==== Copying samples for tests===');
-ncp('./src/test/standard/json/samples/', './build/test/standard/json/samples/');
-ncp('./src/test/standard/markdown/samples/', './build/test/standard/markdown/samples/');
-console.log('\n');
