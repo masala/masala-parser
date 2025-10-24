17714d2b4361f43ef1794f7384b20d8e36b7dce2
Nicolas Zozol <912634+nicolas-zozol@users.noreply.github.com>
2025-10-24 21:36:58 +0100

    fix(v2): removing raw js integration directory. Too difficult to ensure correctness.

--- integration-npm/README.md
+++
@@ -1,5 +0,0 @@
-# WARNING: Outdated. Use integrztion-ts
-
-This directory needs to be migrated to integration-ts before 1.0
-
-Nevertheless some examples on lazy or recursion may be good 
--- integration-npm/examples/parser/debug.js
+++
@@ -1,9 +0,0 @@
-const { Streams, C } = require('@masala/parser')
-const { assertFalse } = require('../../assert')
-
-const floorCombinator = C.string('Hello')
-    .then(C.string(' World'))
-    .then(C.string('fail'))
-
-const parsing = floorCombinator.parse(Streams.ofChars('Hello World !!!'))
-assertFalse(parsing.isAccepted(), 'Testing debug')
