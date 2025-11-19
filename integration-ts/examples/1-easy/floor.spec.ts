4728529bb83b5a1362fec5476cd0e40e2567e4c3
Nicolas Zozol <912634+nicolas-zozol@users.noreply.github.com>
2025-11-19 08:11:07 +0100

    fix(doc): better documentation

--- integration-ts/examples/1-easy/floor.spec.ts
+++ integration-ts/examples/1-easy/floor.spec.ts
@@ -1,18 +1,19 @@
 import { describe, it, expect } from 'vitest'
 import { Stream, C, N } from '@masala/parser'
 
-describe('Absolute combinator', () => {
+describe('Absolute parser', () => {
     it('should parse a number between pipes and make it positive', () => {
         // We parse the mathematical absolute expression: |-4.6|
         let stream = Stream.ofChars('|-4.6|')
         const absoluteParser = C.char('|')
             .then(N.number())
             .last() // we had ['|',-4.6], we keep -4.6
-            .then(C.char('|').drop()) // we have [-4.6, '|'], we keep [-4.6]
+            // we take and immediately drop '|'
+            .then(C.char('|').drop()) // we now have [-4.6]
             .single()
-            .map((x) => Math.abs(x))
+            .map((x) => Math.abs(x)) // we map -4.6 to 4.6
 
-        // Parsec needs a stream of characters
+        // Masala needs a stream of characters
         let parsing = absoluteParser.parse(stream)
         expect(parsing.value).toBe(4)
     })
